c1b37a406c152a578189bdcdb2ac1c6761e469c8
IJke <ijke@overscope.net>
2018-01-28 13:41:14 +0100

    Everything except timer and restart, cocoa pods didn't work for beta

--- ARKit_Hittesting/ViewController.swift
+++ ARKit_Hittesting/ViewController.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    @IBOutlet weak var play: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.session.run(configuration)
+//        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        let tapGestureReconizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureReconizer)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +30,54 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func play(_ sender: Any) {
+        addNode()
+        self.play.isEnabled = false
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    }
+    
+    func addNode() {
+        let jellyFishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        let jellyFishNode = jellyFishScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
+        jellyFishNode?.position = SCNVector3(randomNumber(firstNum: -1, secondNum: 1), randomNumber(firstNum: -0.5, secondNum: 0.5), randomNumber(firstNum: -1, secondNum: 1))
+        self.sceneView.scene.rootNode.addChildNode(jellyFishNode!)
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty {
+            print("didn't tap anything")
+        } else if hitTest.first?.node.name == "Jellyfish" {
+            let results = hitTest.first!
+            let node = results.node
+            if node.animationKeys.isEmpty {
+                SCNTransaction.begin()
+                self.animateNode(node: node)
+                SCNTransaction.completionBlock = {
+                    node.removeFromParentNode()
+                    self.addNode()
+                }
+                SCNTransaction.commit()
+            }
+        }
+    }
+    
+    func animateNode(node: SCNNode) {
+        let spin = CABasicAnimation(keyPath: "position")
+        spin.fromValue = node.presentation.position
+        spin.toValue = SCNVector3(node.presentation.position.x - 0.2, node.presentation.position.y - 0.2, node.presentation.position.z - 0.2)
+        spin.autoreverses = true
+        spin.duration = 0.07
+        spin.repeatCount = 5
+        node.addAnimation(spin, forKey: "position")
+    }
+    
+    func randomNumber(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
 }
 
